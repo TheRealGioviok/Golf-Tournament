3999b7c8e60ac9fc44102faebb98000cc0a824cd
TheRealGioviok <64363733+TheRealGioviok@users.noreply.github.com>
2021-05-17 21:46:08 +0200

    Multiple syntax fixes

--- headers/list.h
+++ headers/list.h
@@ -1,3 +1,4 @@
+#pragma once
 #include <exception>
 #include <stdio.h>
 #include <string.h>
@@ -12,93 +13,105 @@ struct immutableException : public std::exception {
 
 
 template <class T>
-	class List {
-		int size;
-		int maxSize;
-		int bucketSize
-			T* arr;
+class List {
+	int size;
+	int maxSize;
+	int bucketSize;
+	T* arr;
 
-	public:
+public:
 
-		int getSize() return size;
-		int getMaxSize() return maxSize;
+	int getSize() {
+		return size;
+	}
+	int getMaxSize() {
+		return maxSize;
+	}
 
-		List(int _maxSize = 1, _bucketSize = 10, bool suppressWarning = false) :maxSize(_maxSize), bucketSize(_bucketSize) {
-			if (!suppressWarning && _bucketSize == 0) cout << "WARNING: List's bucket size was set to 0. The List is immutable (List object at " << this << " )" << endl;
-			arr = new T[_maxSize];
-		};
+	List(int _maxSize = 1, int _bucketSize = 10, bool suppressWarning = false) :maxSize(_maxSize), bucketSize(_bucketSize) {
+		if (!suppressWarning && _bucketSize == 0) std::cout << "WARNING: List's bucket size was set to 0. The List is immutable (List object at " << this << " )" << std::endl;
+		arr = new T[_maxSize];
+	};
 
-		void insert(T* elem, int pos = 0) {
-			if (pos < 0) {
-				throw std::invalid_argument("In List::insert : pos argument is negative.");
-			}
-			if (pos >= size) {
-				throw std::out_of_range("In List::pos : list index out of range.")
-			}
-			if (bucketSize == 0) {
-				throw immutableException();
-			}
-			if (size >= maxSize) {
-				maxSize += bucketSize;
+	void insert(T* elem, int pos = 0) {
+		if (pos < 0) {
+			throw std::invalid_argument("In List::insert : pos argument is negative.");
+		}
+		if (pos > size) {
+			throw std::out_of_range("In List::pos : list index out of range.");
+		}
+		if (bucketSize == 0) {
+			throw immutableException();
+		}
+		if (size >= maxSize) {
+			maxSize += bucketSize;
+			T* alloc = new T[maxSize];
+			memcpy(alloc, arr, sizeof(T) * size);
+			T* tmp = arr;
+			arr = alloc;
+			delete[] tmp;
+		}
+		arr[size++] = *elem;
+	}
+	
+	void pop(int pos) {
+		if (pos < 0) {
+			throw std::invalid_argument("In List::remove : pos argument is negative.");
+		}
+		if (pos >= size) {
+			throw std::out_of_range("In List::remove : list index out of range.");
+		}
+		memcpy(arr + sizeof(T) * (pos - 1), arr + sizeof(T) * (pos + 1), sizeof(T) * (size - pos - 1));
+			if (size < maxSize - bucketSize) {
+				maxSize -= 10;
 				T* alloc = new T[maxSize];
-				memcpy(&alloc, &data, sizeof(T) * size);
+				memcpy(alloc, arr, sizeof(T) * maxSize);
 				T* tmp = arr;
 				arr = alloc;
 				delete[] tmp;
 			}
-			arr[size++] = elem;
-		}
+		size--;
+	}
 
-		void remove(int pos) {
-			if (pos < 0) {
-				throw std::invalid_argument("In List::remove : pos argument is negative.");
-			}
-			if (pos >= size) {
-				throw std::out_of_range("In List::remove : list index out of range.")
-			}
-			memcpy(arr, arr + sizeof(T) * (pos + 1), sizeof(T) * (size - pos - 1))
-				if (size < maxSize - bucketSize) {
-					maxSize -= 10;
-					T* alloc = new T[maxSize];
-					memcpy(&alloc, &data, sizeof(T) * maxSize);
-					T* tmp = arr;
-					arr = alloc;
-					delete[] tmp;
-				}
-			size--;
+	int& operator[](int pos) {
+		if (pos < 0) {
+			throw std::invalid_argument("In List::[] : pos argument is negative.");
 		}
-
-		int& operator[](int pos){
-			if (pos < 0) {
-				throw std::invalid_argument("In List::[] : pos argument is negative.");
-			}
-			if (pos >= size) {
-				throw std::out_of_range("In List::[] : list index out of range.")
-			}
-			return arr[pos];
+		if (pos >= size) {
+			throw std::out_of_range("In List::[] : list index out of range.");
 		}
+		return arr[pos];
+	}
 
-		int& operator[] const(int pos) {
-			if (pos < 0) {
-				throw std::invalid_argument("In List::[] : pos argument is negative.");
-			}
-			if (pos >= size) {
-				throw std::out_of_range("In List::[] : list index out of range.")
-			}
-			return arr[pos];
+	/*int& operator[] const(int pos) {
+		if (pos < 0) {
+			throw std::invalid_argument("In List::[] : pos argument is negative.");
 		}
-
-		int find(T elem) {
-			for (int i = 0; i < size; i++) {
-				if (elem == arr[i]) {
-					return i;
-				}
-			}
-			return -1;
+		if (pos >= size) {
+			throw std::out_of_range("In List::[] : list index out of range.");
 		}
+		return arr[pos];
+	}*/
 
-		void push_back(T* elem) insert(elem, size);
-		void pop_back() remove(size-1);
-		void insert(T elem, int pos) insert(new T(elem), pos);
-		void push_back(T elem) insert(new T(elem), size);
-	};
+	int find(T elem) {
+		for (int i = 0; i < size; i++) {
+			if (elem == arr[i]) {
+				return i;
+			}
+		}
+		return -1;
+	}
+
+	void push_back(T* elem) {
+		insert(elem, size);
+	}
+	void pop_back() {
+		pop(size);
+	}
+	void insert(T elem, int pos) {
+		insert(new T(elem), pos);
+	}
+	void push_back(T elem) {
+		insert(new T(elem), size);
+	}
+};
