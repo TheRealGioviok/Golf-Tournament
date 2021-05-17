9d2ffe1dd669e010e32d4f25e171ae1ea21908dd
TheRealGioviok <64363733+TheRealGioviok@users.noreply.github.com>
2021-05-17 23:02:16 +0200

    More test cases YEE

--- main.cpp
+++ main.cpp
@@ -1,8 +1,3 @@
-#include <iostream>
-
-#include "list.h"
-#include "types.h"
-#include <iostream>
 int main() {
 	List<int> list;
 	for (int i = 0; i < 10; i++) {
@@ -11,5 +6,14 @@ int main() {
 	for (int i = 0; i < 10; i++) {
 		std::cout << list[i] << std::endl;
 	}
-	
+
+	List<golf::Data> list2;
+	golf::Data f = golf::Data("Stroustrup", "Bjarne","Danish Golf Club ", 100.0, 1950);
+	golf::Data f2 = golf::Data("Ritchie", "Dennis","US Golf Club", 100.0, 1941);
+
+	List<golf::Data> list3;
+	golf::Reader::populateList(&list3,"test.txt");
+	for (int i = 0; i < list3.getSize(); i++) {
+		std::cout << list3[i] << std::endl;
+	}
 }
